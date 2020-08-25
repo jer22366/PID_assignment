b5d37677013cac86a6f783c0550bf7065af1ba8b
CY0290 <65933458+locklen860118@users.noreply.github.com>
2020-08-25 17:52:56 +0800

    add function

--- pid_assignment/index.php
+++ pid_assignment/index.php
@@ -1,16 +1,16 @@
 <?php
-// require ("config.php");
+require ("config.php");
 
-// $link = mysqli_connect ( $dbhost, $dbuser, $dbpass ) or die ( mysqli_connect_error() );
-// $result = mysqli_query ( $link, "set names utf8" );
-// mysqli_select_db ( $link, $dbname );
-// $commandText = <<<SqlQuery
-// select id, firstName, lastName, title, picture,
-//   (select count(*) from employee where managerId = e.id) as reportCount
-//   from employee e
-// SqlQuery;
+$link = mysqli_connect ( $dbhost, $dbuser, $dbpass ) or die ( mysqli_connect_error() );
+$result = mysqli_query ( $link, "set names utf8" );
+mysqli_select_db ( $link, $dbname );
+$commandText = <<<SqlQuery
+select id, firstName, lastName, title, picture,
+  (select count(*) from employee where managerId = e.id) as reportCount
+  from employee e
+SqlQuery;
 
-// $result = mysqli_query ( $link, $commandText );
+$result = mysqli_query ( $link, $commandText );
 if(isset($_POST["addshopping"])){
 	header("location: shoppingcar.php");
 }
@@ -46,11 +46,41 @@ if(isset($_POST["addshopping"])){
 			<p><?php echo $row["title"] ?> </p> <span class="ui-li-count"><?php echo $row["reportCount"] ?></span>
 		</a>
 		</li>
+		<div class="custom-controls-stacked">
+        <div class="custom-control custom-radio">
+          <input name="radio" id="radio_0" type="radio" class="custom-control-input" value="rabbit"> 
+          <label for="radio_0" class="custom-control-label">Rabbit</label>
+        </div>
+      </div>
 	<?php endwhile ?>
 	</ul>
 </div>
 
 </div>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form>
+  <div class="form-group row">
+    <div class="col-4"></div> 
+    <div class="col-8">
+      
+      <div class="custom-controls-stacked">
+        <div class="custom-control custom-radio">
+          <input name="radio" id="radio_1" type="radio" class="custom-control-input" value="duck"> 
+          <label for="radio_1" class="custom-control-label">Duck</label>
+        </div>
+      </div>
+      <div class="custom-controls-stacked">
+        <div class="custom-control custom-radio">
+          <input name="radio" id="radio_2" type="radio" class="custom-control-input" value="fish"> 
+          <label for="radio_2" class="custom-control-label">Fish</label>
+        </div>
+      </div>
+    </div>
+  </div> 
+</form>
+
 <?php
 mysqli_close ( $link );
 ?>
