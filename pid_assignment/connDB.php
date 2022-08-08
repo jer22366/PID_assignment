4cea263afde505df64ee450faefe2b34274972ac
jer22366 <jer22356@yahoo.com.tw>
2022-08-08 21:11:07 +0800

    FitstStepFinish

--- pid_assignment/connDB.php
+++ pid_assignment/connDB.php
@@ -1,4 +1,4 @@
 <?php
- $link = mysqli_connect("localhost","root","root","shoppingDB")or die ( mysqli_connect_error() );
+ $link = mysqli_connect("localhost","root","","shoppingDB")or die ( mysqli_connect_error() );
  $result=mysqli_query($link,"set names utf-8");
 ?>
